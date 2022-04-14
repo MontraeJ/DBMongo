38ecbe10d8e651b330d22aa8862e7ef549e675e7
unknown <montrae@montraejemison.com>
2022-04-13 21:38:12 -0500

    Cross your fingers

---
+++ routes/products.js
@@ -0,0 +1,17 @@
+const Product = require("../models/product")
+const express = require ("express");
+const router = express.Router();
+
+//endpoints will go here.
+
+router.post("/", async (req, res) => {
+    try {
+        let newProduct = await new Product(req.body)
+        await newProduct.save()
+        return res.status(201).send(newProduct);
+    } catch (error) {
+        return res.status(500).send(`Internal Server Error: ${error}`);
+    }
+})
+
+module.exports = router;
